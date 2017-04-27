3267444b1c18a973344d989162d853ac8718f24b
Darren Haken <darrenhaken@gmail.com>
2017-04-27 10:21:32 +0100

    Implemented an eg using Spring boot

---
+++ spring-boot-sample/src/main/java/com/shinko/Application.java
@@ -0,0 +1,24 @@
+package com.shinko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
