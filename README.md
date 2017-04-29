e6bbaf61408a2b3e9692cb3637170455e21da92b
Darren Haken <darrenhaken@gmail.com>
2017-04-29 18:16:59 +0100

    Connect the Spring boot application to Redis

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # docker-examples
 Docker examples demonstrating it's capabilities with Java applications
+
+## Docker Walkthrough
--- spring-boot-sample/src/main/java/com/shinko/Application.java
+++ spring-boot-sample/src/main/java/com/shinko/Application.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -13,9 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+    private final Jedis jedis;
+    private static final String KEY = "home";
+
+    public Application() {
+        jedis = new Jedis("redis");
+    }
+
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        String home = jedis.get(KEY);
+        return home != null ? home : "Hello Docker World";
+    }
+
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+    public ResponseEntity<?> add(@RequestBody String message) {
+        jedis.set(KEY, message);
+        return ResponseEntity.ok("Value updated");
     }
 
     public static void main(String[] args) {
